89fda7e94cf4b2fb89d883c9514c93677f3651f5
Alan Pierce <alangpierce@gmail.com>
2015-03-15 17:12:20 -0700

    Stop using weak references to keep track of subscribers
    It turns out that we don't actually reference them elsewhere in some cases where
    they're still needed, so the UI was pretty much completely non-functional in the
    emulator.

--- app/src/main/java/com/alangpierce/lambdacalculusplayground/drag/TouchObservableManagerImpl.java
+++ app/src/main/java/com/alangpierce/lambdacalculusplayground/drag/TouchObservableManagerImpl.java
@@ -7,9 +7,10 @@ import com.google.common.collect.ImmutableList;
 
 import java.util.Collections;
 import java.util.List;
-import java.util.Map;
+import java.util.Set;
 import java.util.WeakHashMap;
 
+import autovalue.shaded.com.google.common.common.collect.Maps;
 import rx.Observable;
 import rx.Subscriber;
 
@@ -19,15 +20,16 @@ public class TouchObservableManagerImpl implements TouchObservableManager {
     @Override
     public Observable<MotionEvent> touchObservableForView(View view) {
         if (!cachedEventObservables.containsKey(view)) {
-            final Map<Subscriber<? super MotionEvent>, Void> subscribers =
-                    Collections.synchronizedMap(new WeakHashMap<>());
-            cachedEventObservables.put(
-                    view, Observable.create(observer -> subscribers.put(observer, null)));
+            final Set<Subscriber<? super MotionEvent>> subscribers =
+                    Collections.newSetFromMap(
+                            Maps.<Subscriber<? super MotionEvent>, Boolean>newConcurrentMap());
+            cachedEventObservables.put(view, Observable.create(
+                    (Observable.OnSubscribe<MotionEvent>) subscribers::add));
             view.setOnTouchListener((v, event) -> {
                 // Copy the list of subscribers first, since onNext might end up modifying the list.
                 // TODO: Handle unsubscribe and actually unsubscribe when necessary.
                 List<Subscriber<? super MotionEvent>> subscriberList =
-                        ImmutableList.copyOf(subscribers.keySet());
+                        ImmutableList.copyOf(subscribers);
                 for (Subscriber<? super MotionEvent> subscriber : subscriberList) {
                     subscriber.onNext(event);
                 }
