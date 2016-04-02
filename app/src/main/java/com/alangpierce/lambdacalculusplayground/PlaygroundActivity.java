302dc2353220883e4172c62945bf4ecaa0a2b14e
Alan Pierce <alangpierce@gmail.com>
2016-04-02 16:29:39 -0700

    Move options menu code from the activity to the fragment
    It's easier to have everything in one place, especially in the future where we
    may want the action bar to create expressions.

--- app/src/main/java/com/alangpierce/lambdacalculusplayground/PlaygroundActivity.java
+++ app/src/main/java/com/alangpierce/lambdacalculusplayground/PlaygroundActivity.java
@@ -2,16 +2,10 @@ package com.alangpierce.lambdacalculusplayground;
 
 import android.annotation.SuppressLint;
 import android.app.Fragment;
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 
 import rx.plugins.RxJavaErrorHandler;
@@ -53,31 +47,5 @@ public class PlaygroundActivity extends AppCompatActivity {
             Fragment fragment = PlaygroundFragment.create(initialState);
             getFragmentManager().beginTransaction().add(R.id.playground_layout, fragment).commit();
         }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_playground, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == R.id.action_palette) {
-            DrawerLayout drawerRoot = (DrawerLayout) findViewById(R.id.drawer_root_view);
-            if (drawerRoot != null) {
-                if (drawerRoot.isDrawerOpen(GravityCompat.END)) {
-                    drawerRoot.closeDrawer(GravityCompat.END);
-                } else {
-                    drawerRoot.openDrawer(GravityCompat.END);
-                }
-            }
-        } else if (item.getItemId() == R.id.action_view_demo_video) {
-            // TODO: Show the video in the app itself instead of going to YouTube.
-            startActivity(
-                    new Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.demo_video_url))));
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/alangpierce/lambdacalculusplayground/PlaygroundFragment.java
+++ app/src/main/java/com/alangpierce/lambdacalculusplayground/PlaygroundFragment.java
@@ -1,10 +1,15 @@
 package com.alangpierce.lambdacalculusplayground;
 
 import android.app.Fragment;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RelativeLayout;
@@ -37,6 +42,7 @@ public class PlaygroundFragment extends Fragment {
         if (bundle != null) {
             loadFromBundle(bundle);
         }
+        setHasOptionsMenu(true);
     }
 
     @SuppressWarnings("unchecked")
@@ -82,5 +88,29 @@ public class PlaygroundFragment extends Fragment {
     public void onDestroyView() {
         drawerRoot = null;
         super.onDestroyView();
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_playground, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_palette) {
+            if (drawerRoot != null) {
+                if (drawerRoot.isDrawerOpen(GravityCompat.END)) {
+                    drawerRoot.closeDrawer(GravityCompat.END);
+                } else {
+                    drawerRoot.openDrawer(GravityCompat.END);
+                }
+            }
+        } else if (item.getItemId() == R.id.action_view_demo_video) {
+            // TODO: Show the video in the app itself instead of going to YouTube.
+            startActivity(
+                    new Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.demo_video_url))));
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
