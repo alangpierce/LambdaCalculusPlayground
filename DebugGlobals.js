41abdbad1fc8af309a852770478a0a517c0a6395
Alan Pierce <alangpierce@gmail.com>
2016-04-22 12:28:13 -0700

    Update debug globals code to allow modifications from the Chrome console
    Previously, refreshes wouldn't happen until the next call from native code. Now,
    we run a setInterval that runs all the time to force any Chrome-triggered events
    to get flushed.

--- DebugGlobals.js
+++ DebugGlobals.js
@@ -2,10 +2,10 @@
  * Globals we expose on the window object for manual debugging with the Chrome
  * console.
  *
- * TODO: Consider disabling this for release builds.
- *
  * @flow
  */
+
+import {NativeModules} from 'react-native'
 
 import * as actions from './actions'
 import store from './store'
@@ -22,10 +22,24 @@ const makeExpression = (exprString: string) => {
         y: 100,
     };
     store.dispatch(actions.addExpression(screenExpr));
+    refreshNative();
 };
 
-window.DebugGlobals = {
-    store,
-    actions,
-    makeExpression,
-};
+// Only run when we're running in Chrome. We detect this by checking if we're in
+// the debuggerWorker.js web worker, which defines a messageHandlers global.
+if (__DEV__ && window.messageHandlers !== undefined) {
+    window.DebugGlobals = {
+        store,
+        actions,
+        makeExpression,
+    };
+
+    // Set a callback to be run regularly. This ensures that pending calls to
+    // native code are flushed in a relatively timely manner, so we can run
+    // things like redux actions from the Chrome console and have them take
+    // effect immediately.
+    setInterval(() => {}, 100);
+
+    console.log(
+        'Created debug globals. Access them on the DebugGlobals object.');
+}
