1f2e3fc442d698f7f1c69de0fb430f7519096290
Alan Pierce <alangpierce@gmail.com>
2016-04-24 19:22:18 -0700

    Add parseFlow tool to use with WebStorm

---
+++ tools/parseFlow.js
@@ -0,0 +1,11 @@
+const exec = require('child_process').exec;
+
+exec('flow --json', (error, stdout, stderr) => {
+    const results = JSON.parse(stdout);
+    results.errors.forEach((error) => {
+        const messageStr = error.message.map((m) => m.descr).join('  ');
+        const message = error.message[0];
+        console.log(
+            `${message.path}:${message.line}:${message.start} ${messageStr}`);
+    });
+});
