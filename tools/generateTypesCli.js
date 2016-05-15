9bc474f20434513c7702905bfa4a26564af7c130
Alan Pierce <alangpierce@gmail.com>
2016-05-15 16:19:59 -0700

    First pass at moving flow types to a .js.flow file
    The normal .js and the .js.flow file are really similar, but over time, the .js
    file will just have plain dynamic stuff (and maybe not be autogenerated at all)
    and the .js.flow file will only have types.

--- tools/generateTypesCli.js
+++ tools/generateTypesCli.js
@@ -2,12 +2,21 @@
 
 import fs from 'fs'
 
-import {generateTypes, generateWebstormTypes} from './TypeGenerator'
+import {generateTypes, generateFlowTypes, generateWebstormTypes} from './TypeGenerator'
 import typeDefs from '../typeDefs'
 
 const main = () => {
     const typesFileStr = generateTypes(typeDefs);
     fs.writeFile(__dirname + '/../types.js', typesFileStr, (err) => {
+        if (err) {
+            console.log('Error: ' + err);
+        } else {
+            console.log('Wrote types file!');
+        }
+    });
+
+    const flowTypesFileStr = generateFlowTypes(typeDefs);
+    fs.writeFile(__dirname + '/../types.js.flow', flowTypesFileStr, (err) => {
         if (err) {
             console.log('Error: ' + err);
         } else {
