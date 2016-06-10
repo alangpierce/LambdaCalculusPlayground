f14c3b7c4d0036f60c0faf2941fbc561833b2cfb
Alan Pierce <alangpierce@gmail.com>
2016-06-09 20:30:01 -0700

    Add styles for the delete bar

--- DeleteBar.js
+++ DeleteBar.js
@@ -8,8 +8,12 @@
 import React from 'react';
 import {
     Text,
+    View,
 } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
 import StatelessComponent from './StatelessComponent';
+import {TOOLBAR_HEIGHT} from './toolbar';
 
 type DeleteBarProps = {
     isDraggingExpression: boolean,
@@ -18,6 +22,28 @@ export default class DeleteBar extends StatelessComponent<DeleteBarProps> {
     render() {
         const {isDraggingExpression} = this.props;
         const text = isDraggingExpression ? 'Remove' : 'Hide';
-        return <Text>{text}</Text>;
+        return <View
+            style={{
+                backgroundColor: '#888888',
+                position: 'absolute',
+                left: 0,
+                right: 0,
+                top: 0,
+                height: TOOLBAR_HEIGHT,
+                flexDirection: 'row',
+                alignItems: 'center',
+                justifyContent: 'center',
+            }}
+        >
+            <Icon name='clear' size={24} color='white' />
+            <Text
+                style={{
+                    color: 'white',
+                    fontSize: 18,
+                }}
+            >
+                {text}
+            </Text>
+        </View>;
     }
 }
