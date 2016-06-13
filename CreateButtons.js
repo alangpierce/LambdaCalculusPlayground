8d084ebeb90ec75151e4c3b632a5c35a43340363
Alan Pierce <alangpierce@gmail.com>
2016-06-12 17:50:17 -0700

    Add validation for definition and variable names

--- CreateButtons.js
+++ CreateButtons.js
@@ -5,7 +5,8 @@
 
 import React from 'react';
 import {
-    View
+    ToastAndroid,
+    View,
 } from 'react-native';
 import DialogAndroid from 'react-native-dialogs';
 
@@ -30,12 +31,16 @@ export default class CreateButtons extends StatelessComponent<CreateButtonsProps
             input: {
                 allowEmptyInput: false,
                 callback: (varName) => {
-                    // TODO: Validate name.
-                    store.dispatch(t.AddExpression.make(
-                        t.CanvasExpression.make(
-                            t.UserLambda.make(varName, null),
-                            t.CanvasPoint.make(100, 100))
-                    ));
+                    const error = checkDefNameErrors(varName);
+                    if (error != null) {
+                        ToastAndroid.show(error, ToastAndroid.LONG);
+                    } else {
+                        store.dispatch(t.AddExpression.make(
+                            t.CanvasExpression.make(
+                                t.UserLambda.make(varName, null),
+                                t.CanvasPoint.make(100, 100))
+                        ));
+                    }
                 },
             }
         });
@@ -52,11 +57,15 @@ export default class CreateButtons extends StatelessComponent<CreateButtonsProps
                 allowEmptyInput: false,
                 type: 0x00001000, // InputType.TYPE_TEXT_FLAG_CAP_CHARACTERS
                 callback: (defName) => {
-                    // TODO: Validate name.
-                    store.dispatch(t.PlaceDefinition.make(
-                        defName,
-                        t.ScreenPoint.make(100, 100),
-                    ));
+                    const error = checkDefNameErrors(defName);
+                    if (error != null) {
+                        ToastAndroid.show(error, ToastAndroid.LONG)
+                    } else {
+                        store.dispatch(t.PlaceDefinition.make(
+                            defName,
+                            t.ScreenPoint.make(100, 100),
+                        ));
+                    }
                 },
             }
         });
@@ -90,4 +99,40 @@ export default class CreateButtons extends StatelessComponent<CreateButtonsProps
             />
         </View>;
     }
+};
+
+const isLowerCase = (letter: string): boolean => {
+    return letter !== letter.toUpperCase();
+};
+
+/**
+ * Returns an error message if the variable name is invalid, or null if the name
+ * is valid.
+ */
+const checkVarNameErrors = (varName: string): ?string => {
+    if (varName.length > 8) {
+        return 'Variable names can only be up to 8 letters long.';
+    }
+    for (const c of varName) {
+        if (!isLowerCase(c)) {
+            return 'Variable names can only contain lower-case letters.';
+        }
+    }
+    return null;
+};
+
+/**
+ * Returns an error message if the definition name is invalid, or null if the
+ * name is valid.
+ */
+const checkDefNameErrors = (defName: string): ?string => {
+    if (defName.length > 8) {
+        return 'Definition names can only be up to 8 letters long.';
+    }
+    for (const c of defName) {
+        if (isLowerCase(c)) {
+            return 'Definition names can only contain capital letters and symbols.';
+        }
+    }
+    return null;
 };
